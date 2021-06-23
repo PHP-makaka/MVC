91bb6f129ecfea1a97823979661a3cf0d6c0ed5a
Zhen <gurkov1999@mail.ru>
2021-06-23 15:44:02 +0300

    ss

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const homeRouter = require('./routes/homeRouter')
 app.use("/users",userRouter);
 app.use('/',homeRouter)
 
-
+////////////
 
 app.use(function (req,res,next){
     res.status(404).send("page not found")
